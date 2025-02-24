01957e209b24d5e8068d31debd3789261d7ec65e
Gina <ginajeon@utexas.edu>
2025-02-23 22:47:01 -0600

    make initial home page and tab bar

---
+++ MealMentor/HomePageViewController.swift
@@ -0,0 +1,33 @@
+//
+//  HomePageViewController.swift
+//  MealMentor
+//
+//  Created by Gina Jeon on 2/23/25.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    
+    @IBOutlet weak var username: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // TODO set username
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
