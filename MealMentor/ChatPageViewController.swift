df30c3241c2bc8d4779c2730e82ece449be29787
Huyen <hcnguyen02@gmail.com>
2025-02-25 09:58:46 -0600

    add navigation to chat

---
+++ MealMentor/ChatPageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatPageViewController.swift
+//  MealMentor
+//
+//  Created by Huyen Nguyen on 2/24/25.
+//
+
+import UIKit
+
+class ChatPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
