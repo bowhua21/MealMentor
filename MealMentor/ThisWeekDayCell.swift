694bc93155347e46213cf15432744b1862652b1b
Gina <ginajeon@utexas.edu>
2025-03-01 16:26:18 -0600

    add weekly calendar functionality to home page

---
+++ MealMentor/ThisWeekDayCell.swift
@@ -0,0 +1,34 @@
+//
+//  ThisWeekDayCell.swift
+//  MealMentor
+//
+//  Created by Gina Jeon on 3/1/25.
+//
+
+import UIKit
+
+class ThisWeekDayCell: UICollectionViewCell {
+    static let identifier = "ThisWeekDayCell"
+    // day of the week label
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var checkMarkIcon: UIImageView!
+    
+    func configure(with date: Date, isTracked: Bool) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE"  // "EEE" for abbreviated weekday name (Mon, Tue, etc.)
+        let weekdayName = formatter.string(from: date)
+        dayLabel.text = weekdayName  // Set the text as the weekday name
+        // change colors if day is tracked or not
+        if isTracked {
+            contentView.backgroundColor = UIColor(red: 0.2509, green: 0.4588, blue: 0.1764, alpha: 1.0)
+            dayLabel.textColor = .white
+            checkMarkIcon.tintColor = .white
+        }
+        else {
+            contentView.backgroundColor = .white
+            dayLabel.textColor = UIColor(red: 0.2509, green: 0.4588, blue: 0.1764, alpha: 1.0)
+            checkMarkIcon.tintColor = .systemGray4
+        }
+        contentView.layer.cornerRadius = 20
+    }
+}
