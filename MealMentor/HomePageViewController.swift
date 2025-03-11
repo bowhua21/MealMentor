489e679b29fa5215a9c20603907ed46604920b27
Gina <ginajeon@utexas.edu>
2025-03-10 19:34:34 -0500

    refine home pg UI for alpha release, clean up code

--- MealMentor/HomePageViewController.swift
+++ MealMentor/HomePageViewController.swift
@@ -18,18 +18,27 @@ let dummyDataPts: [BarChartDataEntry] = [
     BarChartDataEntry(x: 3.0, y: 30.0),
     BarChartDataEntry(x: 4.0, y: 0.0),
     BarChartDataEntry(x: 5.0, y: 10.0),
-    BarChartDataEntry(x: 6.0, y: 25.0),
+    BarChartDataEntry(x: 6.0, y: 0.0),
     BarChartDataEntry(x: 7.0, y: 40.0)
 ]
 
+let dummyDataPts2: [BarChartDataEntry] = [
+    BarChartDataEntry(x: 1.0, y: 1500.0),
+    BarChartDataEntry(x: 2.0, y: 2000.0),
+    BarChartDataEntry(x: 3.0, y: 1700.0),
+    BarChartDataEntry(x: 4.0, y: 0.0),
+    BarChartDataEntry(x: 5.0, y: 1975.0),
+    BarChartDataEntry(x: 6.0, y: 0.0),
+    BarChartDataEntry(x: 7.0, y: 1530.0)
+]
 
 class HomePageViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDelegate, UITableViewDataSource {
-    
     // home screen properties
     @IBOutlet weak var username: UILabel!
     @IBOutlet weak var numDaysLoggedThisMonth: UILabel!
     @IBOutlet weak var numDayStreak: UILabel!
     @IBOutlet weak var proteinCellButton: UIButton!
+    @IBOutlet weak var caloriesCellButton: UIButton!
     var userName:String = "Jane Doe"
     // each floating cell
     @IBOutlet weak var highlightsView: UIView!
@@ -40,14 +49,31 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
     // today's meals table view within todayMealsView
     let tableViewCellIdentifier = "TableViewCellIdentifier"
     @IBOutlet weak var todayMealsTableView: UITableView!
-  
     // calendar collection within thisWeekView
     @IBOutlet weak var thisWeekCollectionView: UICollectionView!
-    //    TODO tracked days
-//    var trackedDays: [Date] = []
+    // TODO tracked days
+    // var trackedDays: [Date] = []
     var daysOfWeek: [Date] = []
     // graphs
     lazy var proteinBarChartView: BarChartView = {
+        let chartView = BarChartView()
+        chartView.backgroundColor = .clear
+        chartView.rightAxis.enabled = false
+        chartView.leftAxis.axisMinimum = 0
+        chartView.leftAxis.drawGridLinesEnabled = false
+        chartView.leftAxis.enabled = false
+        chartView.xAxis.valueFormatter = BarChartXAxisWeekdayValueFormatter()
+        chartView.xAxis.granularity = 1
+        chartView.xAxis.labelPosition = .bottom
+        chartView.xAxis.axisMinimum = 0.5
+        chartView.xAxis.axisMaximum = 7.5
+        chartView.xAxis.labelCount = 7
+        chartView.xAxis.drawGridLinesEnabled = false
+        chartView.legend.enabled = false
+        
+        return chartView
+    }()
+    lazy var caloriesBarChartView: BarChartView = {
         let chartView = BarChartView()
         chartView.backgroundColor = .clear
         chartView.rightAxis.enabled = false
@@ -75,7 +101,6 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         // Do any additional setup after loading the view.
         // TODO set username
         
-        
         // setup today's meals
         todayMealsTableView.dataSource = self
         todayMealsTableView.delegate = self
@@ -83,7 +108,7 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         // setup this week calendar collection view
         thisWeekCollectionView.dataSource = self
         thisWeekCollectionView.delegate = self
-        thisWeekCollectionView.layer.backgroundColor  = UIColor.clear.cgColor
+        thisWeekCollectionView.layer.backgroundColor = UIColor.clear.cgColor
         let today = Date()
         let startOfWeek = Calendar.current.startOfWeek(for: today)
         daysOfWeek = getDaysOfWeek(startOfWeek: startOfWeek)
@@ -96,6 +121,15 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         setProteinData()
         // make sure protein cell button is still clickable
         weeklyProteinView.bringSubviewToFront(proteinCellButton)
+        
+        // setup calories bar chart
+        weeklyCaloriesView.addSubview(caloriesBarChartView)
+        caloriesBarChartView.center(in: weeklyCaloriesView, offset: CGPoint(x: 0, y: 10))
+        caloriesBarChartView.width(360)
+        caloriesBarChartView.height(130)
+        setCaloriesDate()
+        // make sure calories cell button is still clickable
+        weeklyCaloriesView.bringSubviewToFront(caloriesCellButton)
     }
     
     override func viewDidLayoutSubviews() {
@@ -108,7 +142,6 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         weeklyCaloriesView.layer.cornerRadius = 20
     }
 
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return daysOfWeek.count
     }
@@ -116,7 +149,13 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ThisWeekDayCell.identifier, for: indexPath) as! ThisWeekDayCell
         let date = daysOfWeek[indexPath.row]
-        cell.configure(with: date, isTracked: false)
+        // (dummy configuration) TODO change to tracked days
+        if indexPath.row == 3 || indexPath.row == 5 {
+            cell.configure(with: date, isTracked: false)
+        }
+        else {
+            cell.configure(with: date, isTracked: true)
+        }
         //TODO check if day is tracked
 //        let isTracked = trackedDays.contains { Calendar.current.isDate($0, inSameDayAs: date) }
 //        cell.configure(with: day, isTracked: true)
@@ -154,6 +193,7 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // set data for protein bar chart
     func setProteinData() {
         let set = BarChartDataSet(entries: dummyDataPts, label: "Protein")
         set.valueFormatter = BarChartYValueUnitValueFormatter(unit: "g")
@@ -162,7 +202,17 @@ class HomePageViewController: UIViewController, UICollectionViewDelegate, UIColl
         proteinBarChartView.data = data
     }
     
+    // set data for calories bar chart
+    func setCaloriesDate() {
+        let set = BarChartDataSet(entries: dummyDataPts2, label: "Calories")
+        set.valueFormatter = BarChartYValueUnitValueFormatter(unit: "cal")
+        set.colors = [UIColor(red: 0.1019, green: 0.4823, blue: 0.8235, alpha: 1.0)]
+        let data = BarChartData(dataSet: set)
+        caloriesBarChartView.data = data
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // have a different segment selected depending on which segue was triggered
         if segue.identifier == segueToProteinVisualizationsIdentifier, let visualizationsVC = segue.destination as? VisualizationsPageViewController {
             // set segmented to be on protein
             visualizationsVC.selectedSegmentIndex = 0
